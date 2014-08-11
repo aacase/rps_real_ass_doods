ab6795e4a93acb33db54ae97237f5aa3f8d16dae
Aaron Case <aacase@gmail.com>
2014-08-11 10:05:18 -0500

    added winner function

---
+++ untitled.rb
@@ -0,0 +1,5 @@
+c8 = ["carl", "brian", "joey", "austin", "justin b", "alli", "joe", "randy", "chris o", "catherine", "derek", "emma", "quin", "dan cox", "dan turner", "justin l", "aaron"]
+rando = c8.shuffle
+pod_1 = rando[0..5]
+pod_2 = rando[6..11]
+pod_3 = rando[12..-1]
